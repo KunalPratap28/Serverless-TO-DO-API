bf637aee19aec5184e7e7cf94e700d4882124659
KunalPratap28 <kunal.p@consultadd.com>
2025-01-28 20:59:07 +0530

    create ToDo API code changes

---
+++ src/create_todo/service.py
@@ -0,0 +1,18 @@
+import os
+import uuid
+import boto3
+
+dynamodb=boto3.resource("dynamodb")
+table_name=os.environ["TABLE_NAME"]
+table=dynamodb.Table(table_name)
+
+def create_todo_item(data):
+    item_id=str(uuid.uuid4())
+    todo_item={
+        "id":item_id,
+        "title":data.get("title"),
+        "description":data.get("description"),
+        "isCompleted":1 if data.get("isCompleted") == "Y" else 0
+    }
+    table.put_item(Item=todo_item)
+    return todo_item
